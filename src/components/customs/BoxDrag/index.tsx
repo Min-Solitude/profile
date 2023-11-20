45d9b9f7b81e4ff5a4e6e5f862d0419972b94f71
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-20 12:00:30 +0700

    udpate

--- src/components/customs/BoxDrag/index.tsx
+++ src/components/customs/BoxDrag/index.tsx
@@ -11,15 +11,15 @@ type BoxDragProps = {
 export default function BoxDrag({ children, className }: BoxDragProps) {
     return (
         <motion.div className={`cursor-pointer ${className}`}
-            drag
-            dragConstraints={{
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-            }}
-            dragElastic={0.1}
-            dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+        // drag
+        // dragConstraints={{
+        //     top: 0,
+        //     left: 0,
+        //     right: 0,
+        //     bottom: 0,
+        // }}
+        // dragElastic={0.1}
+        // dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
         >
             {children}
         </motion.div>
